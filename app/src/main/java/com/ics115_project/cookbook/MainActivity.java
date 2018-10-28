446e2a1898a1a1d6faba26907940cba492d88d08
luiscaringal <2015081629@ust-ics.mygbiz.com>
2018-10-28 21:33:40 +0800

    Added loading auto transition before the login page

--- app/src/main/java/com/ics115_project/cookbook/MainActivity.java
+++ app/src/main/java/com/ics115_project/cookbook/MainActivity.java
@@ -1,14 +1,30 @@
 package com.ics115_project.cookbook;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.load_page);
         getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent i = new Intent(MainActivity.this, login_activity.class);
+                MainActivity.this.startActivity(i);
+                MainActivity.this.finish();
+            }
+        }, 5000);
+
     }
 }
