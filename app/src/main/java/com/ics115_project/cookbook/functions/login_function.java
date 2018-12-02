82e541b6bdba43d04b9f60d22788a86aca29541f
luiscaringal <2015081629@ust-ics.mygbiz.com>
2018-12-02 23:44:18 +0800

    converted to firebase

--- app/src/main/java/com/ics115_project/cookbook/functions/login_function.java
+++ app/src/main/java/com/ics115_project/cookbook/functions/login_function.java
@@ -1,75 +1,18 @@
 package com.ics115_project.cookbook.functions;
 
-import android.content.Context;
-import android.os.AsyncTask;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import org.json.*;
 
+import android.util.Log;
 
-public class login_function extends AsyncTask <String,Void,String> {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    Context context;
+public class login_function {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
 
-    public login_function(Context context){
-        this.context = context;
-    }
+    
 
-    @Override
-    protected String doInBackground(String... voids) {
-        String result = "";
-        String username = voids[0];
-        String password = voids[1];
-
-        host_ip host = new host_ip();
-
-        String link = "http://" + host.getHostIp() + "/android_api/login_user.php";
-
-        try {
-            URL url = new URL(link);
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setRequestMethod("POST");
-            http.setDoInput(true);
-            http.setDoOutput(true);
-
-            OutputStream ops = http.getOutputStream();
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ops, "UTF-8"));
-            String data =   URLEncoder.encode("username","UTF-8") + "=" + URLEncoder.encode(username,"UTF-8") + "&&" +
-                            URLEncoder.encode("password","UTF-8") + "=" + URLEncoder.encode(password,"UTF-8");
-
-            writer.write(data);
-            writer.flush();
-            writer.close();
-            ops.close();
-
-            InputStream ips = http.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(ips,"ISO-8859-1"));
-            String line = "";
-            while((line = reader.readLine()) != null){
-                JSONObject obj = new JSONObject(line);
-                result = obj.getString("success");
-            };
-
-            reader.close();
-            ips.close();
-            http.disconnect();
-            return result;
-        } catch (MalformedURLException e) {
-            result = e.getMessage();
-        } catch (IOException e) {
-            result = e.getMessage();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
 }
