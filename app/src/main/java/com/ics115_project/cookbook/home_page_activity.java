17ee50d5639eb027ce04f73805ed19fdbad6fe4c
odettedator <datorodette@gmail.com>
2018-12-09 17:05:53 +0800

    Merge branch 'master' of https://github.com/luiscaringal/ICS115-Project-Cookbook
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/res/layout/home_page_layout.xml
    #	app/src/main/res/layout/profile_layout.xml

--- app/src/main/java/com/ics115_project/cookbook/home_page_activity.java
+++ app/src/main/java/com/ics115_project/cookbook/home_page_activity.java
@@ -1,21 +1,78 @@
 package com.ics115_project.cookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class home_page_activity extends AppCompatActivity {
+    private DrawerLayout drawer;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page_layout);
+        setContentView(R.layout.home_page);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() == null) {
+            startActivity(new Intent(this, login_user_activity.class));
+        }
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.home:
+                            selectedFragment = new Fragment();
+                            break;
+                        case R.id.profile:
+                            selectedFragment = new profile_fragment();
+                            break;
+                        case R.id.order:
+                            selectedFragment = new order_fragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, login_user_activity.class));
+        }
+        return true;
     }
 }
--- app/src/main/java/com/ics115_project/cookbook/login_user_activity.java
+++ app/src/main/java/com/ics115_project/cookbook/login_user_activity.java
@@ -1,52 +1,85 @@
 package com.ics115_project.cookbook;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
-import com.ics115_project.cookbook.functions.login_function;
-
-import java.util.concurrent.ExecutionException;
-
-public class login_user_activity extends AppCompatActivity {
-    EditText uname, pass;
+public class login_user_activity extends AppCompatActivity implements View.OnClickListener {
+    private EditText loginEmail, loginPassword;
+    private Button login, signup;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-        uname = findViewById(R.id.login_username);
-        pass = findViewById(R.id.login_password);
+        setContentView(R.layout.login_page);
+
+        loginEmail = (EditText) findViewById(R.id.login_email);
+        loginPassword = (EditText) findViewById(R.id.login_password);
+        login = (Button) findViewById(R.id.button_login);
+        signup = (Button) findViewById(R.id.button_signup);
+
+        progressDialog = new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            startActivity(new Intent(this, home_page_activity.class));
+            finish();
+        }
+
+        login.setOnClickListener(this);
+        signup.setOnClickListener(this);
     }
 
-    public void signUp(View v){
-        Intent i = new Intent(this, register_user_activity.class);
-        startActivity(i);
+    private void userLogin() {
+        String email = loginEmail.getText().toString().trim();
+        String password = loginPassword.getText().toString().trim();
+
+        if (email.length() == 0 && password.length() == 0) {
+            Toast.makeText(login_user_activity.this, "Please enter email and password", Toast.LENGTH_LONG).show();
+        } else if (email.length() == 0) {
+            Toast.makeText(login_user_activity.this, "Please enter email", Toast.LENGTH_LONG).show();
+        } else if (password.length() == 0) {
+            Toast.makeText(login_user_activity.this, "Please enter password", Toast.LENGTH_LONG).show();
+        } else {
+            progressDialog.setMessage("Logging in");
+            progressDialog.show();
+
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
+                            progressDialog.dismiss();
+                            if (task.isSuccessful()) {
+                                Toast.makeText(login_user_activity.this, "Login Success", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(login_user_activity.this, home_page_activity.class));
+                                finish();
+                            } else {
+                                Toast.makeText(login_user_activity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
     }
 
-    public void login(View v){
-        String username = uname.getText().toString();
-        String password = pass.getText().toString();
-        login_function login = new login_function(this);
-        try {
-            String result = login.execute(username,password).get();
-
-            if(result.equals("1")){
-                Toast.makeText(this,"Login Success", Toast.LENGTH_LONG).show();
-                Intent i = new Intent(this, home_page_activity.class);
-                startActivity(i);
-            }
-            else{
-                Toast.makeText(this,"Login Unsuccessful", Toast.LENGTH_LONG).show();
-            }
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    @Override
+    public void onClick(View v) {
+        if (v == login) {
+            userLogin();
+        }
+        if (v == signup) {
+            startActivity(new Intent(this, register_user_activity.class));
+            finish();
         }
     }
 }
