ded99973001c664ea1cc7a65be2cca52a0eb87a5
luiscaringal <2015081629@ust-ics.mygbiz.com>
2018-12-10 18:26:38 +0800

    added edit profile and add food layout

--- app/src/main/java/com/ics115_project/cookbook/ProfileFragment.java
+++ app/src/main/java/com/ics115_project/cookbook/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.ics115_project.cookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ProfileFragment extends Fragment {
+    Button addFood;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+        addFood = (Button) view.findViewById(R.id.button_add_food);
+
+        addFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileFragment.this.getActivity() ,AddFoodActivity.class));
+            }
+        });
+
+        return view;
     }
 }
